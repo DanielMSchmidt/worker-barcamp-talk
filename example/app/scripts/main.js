7db70e9075314d7a081134cffff852c3e7799216
Daniel Schmidt <dschmidt@weluse.de>
2015-07-25 23:55:53 +0200

    wrote basic react components to display products

--- example/app/scripts/main.js
+++ example/app/scripts/main.js
@@ -1,3 +1,64 @@
-$(function() {
+'use strict';
 
+var Product = React.createClass({
+  propTypes: {
+    obj: React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      price: React.PropTypes.string.isRequired,
+      shop_id: React.PropTypes.number.isRequired,
+      primary_image: React.PropTypes.shape({
+        url: React.PropTypes.string,
+      })
+    }).isRequired
+  },
+
+  renderImage: function() {
+    if (this.props.obj.primary_image) {
+      return (
+        <img src={this.props.obj.primary_image.url} />
+      );
+    } else {
+      return (<img />);
+    }
+  },
+
+  render: function () {
+    return (
+      <div className="product">
+        <h3>{ this.props.obj.name } <span>{ this.props.obj.price } €</span></h3>
+        {this.renderImage()}
+      </div>
+    );
+  }
 });
+
+var App = React.createClass({
+  propTypes: {
+    content: React.PropTypes.array
+  },
+  render: function () {
+    return (
+      <div className="stream">
+        { this.props.content.map(function(product){
+          return <Product obj={product} />;
+        }) }
+      </div>
+    );
+  }
+});
+
+var render = function (products) {
+  React.render(
+    <App content={products} />,
+    document.getElementById('container')
+  );
+};
+
+$(function() {
+  var l = window.loader;
+  render([]);
+
+  l.getNextPage().then(function() {
+    render(l.getAllProducts());
+  });
+})
